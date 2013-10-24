2e738f077af66aee16524391279e4475747ab2a9
Jeff Ober <jeffober@gmail.com>
2013-10-24 11:08:56 -0400

    Initial commit

---
+++ denim/protocol.py
@@ -0,0 +1,65 @@
+try:
+    import cPickle as pickle
+except:
+    import pickle
+
+import base64
+import uuid
+
+
+class Task(object):
+    def __init__(self, f, args=None, kwargs=None):
+        if args is None:
+            args = []
+
+        if kwargs is None:
+            kwargs = {}
+
+        self.f = f
+        self.args = args
+        self.kwargs = kwargs
+        self.result = None
+        self.is_error = False
+
+    def perform(self):
+        try:
+            self.result = self.f(*self.args, **self.kwargs)
+        except Exception, e:
+            self.result = e
+            self.is_error = True
+
+    def get_result(self):
+        if self.is_error:
+            raise self.result
+        else:
+            return self.result
+
+
+class Msg(object):
+    ERR = 'ERR'
+    ACK = 'ACK'
+
+    def __init__(self, cmd, msgid=None, payload=None):
+        if msgid is None:
+            msgid = str(uuid.uuid4())
+
+        self.cmd = cmd
+        self.msgid = msgid
+        self.payload = payload
+
+    def encode(self):
+        return "%s|%s|%s\n" % (
+            self.cmd,
+            self.msgid,
+            base64.b64encode(pickle.dumps(self.payload)),
+        )
+
+    @staticmethod
+    def decode(line):
+        (cmd, msgid, data) = line.split('|')
+        payload = pickle.loads(base64.b64decode(data))
+        return Msg(cmd, msgid, payload)
+
+    def reply(self, cmd, payload=None):
+        return Msg(cmd, self.msgid, payload=payload)
+
